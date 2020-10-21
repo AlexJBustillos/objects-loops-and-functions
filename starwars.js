35790a5c72ec5a4f7338800185ab888560ca3470
alexjbustillos <alexjbustillos@gmail.com>
2020-10-21 15:37:37 -0400

    [objects] changes to starwars and iphone12

---
+++ starwars.js
@@ -0,0 +1,12 @@
+const starwars = {
+    image: 'star wars image url',
+    length: '1:00',
+    title: 'Special Look the Mandolorian',
+    username: 'Star Wars',
+    views: '1M',
+    uploaded: '1 day ago'
+
+
+}
+
+console.log(starwars.title);
